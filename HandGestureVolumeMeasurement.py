86372f5a2f9e7cd3201175af7779b46779bd646b
Tathya Dixit <dixit.tathya2003@gmail.com>
2022-10-26 21:55:51 +0530

    Third commit In this commit i have added volume measurement in the xy plane. Now it can measure volume in xy plane and x and y axes differently as well.

--- HandGestureVolumeMeasurement.py
+++ HandGestureVolumeMeasurement.py
@@ -34,26 +34,41 @@ while True:
             mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
             diffx = lst[4][0]-lst[8][0]
             diffy = lst[4][1]-lst[8][1]
-            if(diffx>250):
+            diff = (diffx**2+diffy**2)**0.5
+            if(diffx>300):
                 perx = 100
             elif(diffx<0):
                 perx=0
             else:
-                perx = (diffx/250)*100
+                perx = (diffx/300)*100
             
-            if(diffy>250):
+            if(diffy>300):
                 pery = 100
             elif(diffy<0):
                 pery=0
             else:
-                pery = (diffy/250)*100
+                pery = (diffy/300)*100
+            
+            if(diff>300):
+                per = 100
+            elif(diff<0):
+                per=0
+            else:
+                per = (diff/300)*100
 
-            cv2.putText(img,"X:"+str(int(perx))+"%",(w-70,160),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),2)
-            cv2.rectangle(img,(w-30,130),(w-60,30),(0,0,0),3)
-            cv2.rectangle(img,(w-30,130),(w-60,130-int(perx)),(255,0,0),-1)
-            cv2.putText(img,"Y:"+str(int(pery))+"%",(w-120,160),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0),2)
-            cv2.rectangle(img,(w-70,130),(w-100,30),(0,0,0),3)
-            cv2.rectangle(img,(w-70,130),(w-100,130-int(pery)),(0,0,255),-1)
+            cv2.rectangle(img,(w,170),(w-240,0),(255,255,255),-1)
+            cv2.putText(img,"--DATA--",(w-170,20),cv2.FONT_HERSHEY_PLAIN,1.3,(0,0,0),2)
+            cv2.putText(img,"X:"+str(int(perx))+"%",(w-70,160),cv2.FONT_HERSHEY_PLAIN,1.3,(0,0,0),2)
+            cv2.rectangle(img,(w-10,130),(w-70,130-int(perx)),(255,0,0),-1)
+            cv2.rectangle(img,(w-10,130),(w-70,30),(0,0,0),2)
+            cv2.putText(img,"Y:"+str(int(pery))+"%",(w-150,160),cv2.FONT_HERSHEY_PLAIN,1.3,(0,0,0),2)
+            cv2.rectangle(img,(w-80,130),(w-150,130-int(pery)),(0,0,255),-1)
+            cv2.rectangle(img,(w-80,130),(w-150,30),(0,0,0),2)
+
+            cv2.putText(img,"V:"+str(int(per))+"%",(w-230,160),cv2.FONT_HERSHEY_PLAIN,1.3,(0,0,0),2)
+            cv2.rectangle(img,(w-160,130),(w-230,130-int(per)),(0,255,0),-1)
+            cv2.rectangle(img,(w-160,130),(w-230,30),(0,0,0),2)
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
